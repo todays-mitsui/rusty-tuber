f86f3e23736f9a90ee561d82f1f09ef07d8eb6c1
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-07-16 23:03:37 +0900

    test: テスト追記

--- src/expr/apply.rs
+++ src/expr/apply.rs
@@ -24,3 +24,42 @@ impl Expr {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::func::Func;
+
+    #[test]
+    fn test_arity() {
+        let f0 = Func::new(vec![], ":a".into());
+        let f1 = Func::new(vec!["x".into()], ":a".into());
+        let f2 = Func::new(vec!["x".into(), "y".into()], ":a".into());
+        let f3 = Func::new(vec!["x".into(), "y".into(), "z".into()], ":a".into());
+
+        let env = Env::from(vec![("f0".into(), f0), ("f1".into(), f1), ("f2".into(), f2), ("f3".into(), f3)]);
+
+        // シンボルは関数が紐づくことがない、arity は定義されない
+        assert_eq!(Expr::s("a").arity(&env), None);
+
+        // 関数適用の arity は定義されない
+        assert_eq!(Expr::a(Expr::v("x"), Expr::v("y")).arity(&env), None);
+
+        // ラムダ抽象の arity は常に 1
+        assert_eq!(Expr::l("x".into(), Expr::v("x")).arity(&env), Some(1));
+
+        // 関数として定義されていない自由変数の arity は定義されない (0ですらない)
+        assert_eq!(Expr::v("x").arity(&env), None);
+
+        // 定義済み関数と紐づく自由変数はその関数の arity を返す
+        assert_eq!(Expr::v("f0").arity(&env), Some(0));
+        assert_eq!(Expr::v("f1").arity(&env), Some(1));
+        assert_eq!(Expr::v("f2").arity(&env), Some(2));
+        assert_eq!(Expr::v("f3").arity(&env), Some(3));
+    }
+
+    #[test]
+    fn test_apply() {
+        // TODO
+    }
+}
