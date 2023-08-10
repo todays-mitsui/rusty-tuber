110b20f5ad270bc6cd9041f0bfbb8080c8b4b9aa
Mitsui <todays.mitsui@gmail.com>
2023-08-10 19:00:35 +0900

    fix: テスト追記、バグ修正

--- src/expression/unlambda.rs
+++ src/expression/unlambda.rs
@@ -16,7 +16,8 @@ impl Expr {
         match self {
             Expr::Variable(id) if &id == param => "i".into(),
             Expr::Variable(id) => Expr::a("k".into(), Expr::Variable(id)),
-            Expr::Symbol(_) => self,
+            Expr::Symbol(_) => Expr::a("k".into(), self),
+            Expr::Apply { .. } if !free_vars(&self).contains(param) => Expr::a("k".into(), self),
             Expr::Apply { lhs, rhs } => match rhs.as_ref() {
                 Expr::Variable(id) if id == param && !free_vars(lhs.as_ref()).contains(param) => {
                     *lhs
@@ -37,17 +38,73 @@ mod tests {
 
     #[test]
     fn test_unlambda() {
+        // x == x
         assert_eq!(Expr::Variable("x".into()).unlambda(), "x".into());
+
+        // :x = :x
         assert_eq!(Expr::Symbol("x".into()).unlambda(), ":x".into());
+
+        // `xy == `xy
         assert_eq!(
             Expr::a("x".into(), "y".into()).unlambda(),
             Expr::a("x".into(), "y".into())
         );
+
+        // ^x.x == i
         assert_eq!(Expr::l("x".into(), "x".into()).unlambda(), "i".into());
+
+        // ^x.:x == `k:x
+        assert_eq!(
+            Expr::l("x".into(), ":x".into()).unlambda(),
+            Expr::a("k".into(), ":x".into())
+        );
+
+        // ^x.y == `ky
         assert_eq!(
             Expr::l("x".into(), "y".into()).unlambda(),
             Expr::a("k".into(), "y".into())
         );
+
+        // ^x.:y == `k:y
+        assert_eq!(
+            Expr::l("x".into(), ":y".into()).unlambda(),
+            Expr::a("k".into(), ":y".into())
+        );
+
+        // ^x.`yx == y
+        assert_eq!(
+            Expr::l("x".into(), Expr::a("y".into(), "x".into())).unlambda(),
+            "y".into()
+        );
+
+        // ^x.`y:x == `k`y:x
+        assert_eq!(
+            Expr::l("x".into(), Expr::a("y".into(), ":x".into())).unlambda(),
+            Expr::a("k".into(), Expr::a("y".into(), ":x".into()))
+        );
+
+        // ^x.`xy == ``si`ky
+        assert_eq!(
+            Expr::l("x".into(), Expr::a("x".into(), "y".into())).unlambda(),
+            Expr::a(
+                Expr::a("s".into(), "i".into()),
+                Expr::a("k".into(), "y".into())
+            )
+        );
+
+        // ^x.`:xy == `k`:xy
+        assert_eq!(
+            Expr::l("x".into(), Expr::a(":x".into(), "y".into())).unlambda(),
+            Expr::a("k".into(), Expr::a(":x".into(), "y".into()))
+        );
+
+        // ^x.`yz == `k`yz
+        assert_eq!(
+            Expr::l("x".into(), Expr::a("y".into(), "z".into())).unlambda(),
+            Expr::a("k".into(), Expr::a("y".into(), "z".into()))
+        );
+
+        // ^x.^y.`xy == i
         assert_eq!(
             Expr::l(
                 "x".into(),
